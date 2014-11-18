3e107c4bbb8be06987954ce68a05774516de0d49
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-11-18 18:26:48 +0100

    Updated to new LIMBO package structure

--- plugins/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/UsageevolutionEditPlugin.java
+++ plugins/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/UsageevolutionEditPlugin.java
@@ -2,106 +2,90 @@
  */
 package org.scaledl.usageevolution.provider;
 
-import de.uka.ipd.sdq.identifier.provider.IdentifierEditPlugin;
-
-import de.uka.ipd.sdq.pcm.core.provider.PalladioComponentModelEditPlugin;
-
-import de.uka.ipd.sdq.probfunction.provider.ProbabilityFunctionEditPlugin;
-
-import de.uka.ipd.sdq.stoex.provider.StoexEditPlugin;
-
-import de.uka.ipd.sdq.units.provider.UnitsEditPlugin;
-
-import dlim.provider.DlimEditPlugin;
-
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
 
+import tools.descartes.dlim.provider.DlimEditPlugin;
+import de.uka.ipd.sdq.identifier.provider.IdentifierEditPlugin;
+import de.uka.ipd.sdq.pcm.core.provider.PalladioComponentModelEditPlugin;
+import de.uka.ipd.sdq.probfunction.provider.ProbabilityFunctionEditPlugin;
+import de.uka.ipd.sdq.stoex.provider.StoexEditPlugin;
+import de.uka.ipd.sdq.units.provider.UnitsEditPlugin;
+
 /**
- * This is the central singleton for the Usageevolution edit plugin.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the central singleton for the Usageevolution edit plugin. <!-- begin-user-doc --> <!--
+ * end-user-doc -->
+ * 
  * @generated
  */
 public final class UsageevolutionEditPlugin extends EMFPlugin {
-	/**
-     * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
-	public static final UsageevolutionEditPlugin INSTANCE = new UsageevolutionEditPlugin();
+    public static final UsageevolutionEditPlugin INSTANCE = new UsageevolutionEditPlugin();
 
-	/**
-     * Keep track of the singleton.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
-	private static Implementation plugin;
+    private static Implementation plugin;
 
-	/**
-     * Create the instance.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
-	public UsageevolutionEditPlugin() {
-        super
-          (new ResourceLocator [] {
-             DlimEditPlugin.INSTANCE,
-             IdentifierEditPlugin.INSTANCE,
-             PalladioComponentModelEditPlugin.INSTANCE,
-             ProbabilityFunctionEditPlugin.INSTANCE,
-             StoexEditPlugin.INSTANCE,
-             UnitsEditPlugin.INSTANCE,
-           });
+    public UsageevolutionEditPlugin() {
+        super(new ResourceLocator[] { DlimEditPlugin.INSTANCE, IdentifierEditPlugin.INSTANCE,
+                PalladioComponentModelEditPlugin.INSTANCE, ProbabilityFunctionEditPlugin.INSTANCE,
+                StoexEditPlugin.INSTANCE, UnitsEditPlugin.INSTANCE, });
     }
 
-	/**
-     * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     * 
      * @return the singleton instance.
      * @generated
      */
-	@Override
-	public ResourceLocator getPluginResourceLocator() {
+    @Override
+    public ResourceLocator getPluginResourceLocator() {
         return plugin;
     }
 
-	/**
-     * Returns the singleton instance of the Eclipse plugin.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     * 
      * @return the singleton instance.
      * @generated
      */
-	public static Implementation getPlugin() {
+    public static Implementation getPlugin() {
         return plugin;
     }
 
-	/**
-     * The actual implementation of the Eclipse <b>Plugin</b>.
-     * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+    /**
+     * The actual implementation of the Eclipse <b>Plugin</b>. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     * 
      * @generated
      */
-	public static class Implementation extends EclipsePlugin {
-		/**
-         * Creates an instance.
-         * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
+    public static class Implementation extends EclipsePlugin {
+        /**
+         * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+         * 
          * @generated
          */
-		public Implementation() {
+        public Implementation() {
             super();
 
             // Remember the static instance.
             //
             plugin = this;
         }
-	}
+    }
 
 }
